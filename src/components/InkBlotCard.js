efe6ff508c3b52f4c2212a3e0fe9597910957fd3
Asif R Choudhury <choudhury.asif@gmail.com>
2017-07-03 19:05:53 -0500

    Tie description text to state

--- src/components/InkBlotCard.js
+++ src/components/InkBlotCard.js
@@ -5,13 +5,28 @@ import { fetchNext } from '../actions/next'
 
 
 class InkBlot extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state =  {
+      url: props.url,
+      description: "What are your thoughts on this?",
+    }
+    this.handleChange = this.handleChange.bind(this);
+  };
+
+  handleChange(event) {
+    this.setState(Object.assign({}, this.state, {
+      description: event.target.value
+    }));
+  };
+
   render() {
     return (
       <div className="inkblotcard">
         <div className="inkblot">
           <img src={this.props.url} alt="inkblot"/>
         </div>
-        <textarea></textarea>
+        <textarea value={this.state.description} onChange={this.handleChange}></textarea>
         <button onClick={this.props.loadNext}>Next</button>
       </div>
     );
