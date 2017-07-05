ae46a3045e19c2c41c6ddfee4483642ec1f60417
Asif R Choudhury <choudhury.asif@gmail.com>
2017-07-04 23:53:25 -0500

    Add isotope and some really bad css

--- src/components/InkBlotCard.js
+++ src/components/InkBlotCard.js
@@ -3,20 +3,20 @@ import { connect } from 'react-redux'
 
 import { fetchNext } from '../actions/next'
 import { saveResponse } from '../actions/responses'
+import './InkBlotCard.css'
 
 
 class InkBlot extends React.Component {
   constructor(props) {
     super(props)
     this.state =  {
-      description: "What are your thoughts on this?",
+      description: "",
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   };
 
   handleSubmit(event) {
-    console.log(this.state, this.props);
     this.props.saveResponse({
       url: this.props.url,
       description: this.state.description
@@ -35,13 +35,19 @@ class InkBlot extends React.Component {
 
   render() {
     return (
-      <div className="inkblotcard">
+      <div className="inkblotcard survey">
+        <div>
+          <button onClick={this.props.loadNext}>Next</button>
+        </div>
         <div className="inkblot">
           <img src={this.props.url} alt="inkblot"/>
         </div>
-        <textarea value={this.state.description} onChange={this.handleChange}></textarea>
-        <button onClick={this.handleSubmit}>Respond</button>
-        <button onClick={this.props.loadNext}>Next</button>
+        <div>
+          <textarea value={this.state.description} onChange={this.handleChange} placeholder="Thoughts?"></textarea>
+        </div>
+        <div>
+          <button onClick={this.handleSubmit}>Respond</button>
+        </div>
       </div>
     );
   }
--- src/components/ResponseList.js
+++ src/components/ResponseList.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import Masonry from 'react-masonry-component';
 
 import { fetchAll } from '../actions/responses'
 
+const randomRotation = () =>  {
+  const index = Math.floor(Math.random()*6) - 3;
+  return {
+    transform: "rotate(" + index + "deg)",
+  };
+}
 
 class Responses extends React.Component {
   render() {
     return (
       <div className="responses">
-        <button onClick={this.props.loadAll}>Refresh</button>
+        <div>
+          <button onClick={this.props.loadAll} className="centerButton">View Responses</button>
+        </div>
+        <Masonry>
         {this.props.responses.inkblots.map( blot => (
-           <div className="inkblotcard" key={blot.id}>
+           <div className="inkblotcard" style={randomRotation()} key={blot.id}>
              <div className="inkblot">
                <img src={blot.url} alt="inkblot"/>
              </div>
              <p>{blot.description}</p>
            </div>
         ))}
+        </Masonry>
       </div>
     );
   }
